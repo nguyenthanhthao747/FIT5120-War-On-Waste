b25bbdeaf681fe9ead70830b9ab884f6320985c6
TT <nguyenthanhthao747@gmail.com>
2019-08-01 01:33:50 +1000

    update setting

--- ConstructionWaste/ConstructionWaste/settings.py
+++ ConstructionWaste/ConstructionWaste/settings.py
@@ -74,27 +74,34 @@ WSGI_APPLICATION = 'ConstructionWaste.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ConstructionWaste',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'ConstructionWaste',
 #         'USER': 'power_user',
-#         'PASSWORD': 'PostAdmin',
-#         'HOST': 'vic-construction-waste.com',
+#
+#         # server host database
+#         # 'PASSWORD': 'PostAdmin',
+#         # 'HOST': 'localhost',
+#
+#         #local host databse
+#         # 'USER': 'postgres',
+#         # 'PASSWORD': 'postgres',
 #         'PORT': '5432',
 #     }
 # }
+
+#connect database on aws
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ConstructionWaste',
+        'USER': 'power_user',
+        'PASSWORD': 'PostAdmin',
+        'HOST': 'vic-construction-waste.com',
+        'PORT': '5432',
+    }
+}
 
 """
 DATABASES = {
